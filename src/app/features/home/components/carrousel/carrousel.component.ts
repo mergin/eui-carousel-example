7806413c8ce6407139636fe88fe31e10d30e8ff3
David <merginvalhala@gmail.com>
2018-04-24 09:02:44 +0200

    Home Carrousel: items backgrounds only on selected item, darker

--- src/app/features/home/components/carrousel/carrousel.component.ts
+++ src/app/features/home/components/carrousel/carrousel.component.ts
@@ -20,10 +20,12 @@ export class HomeCarrouselComponent implements OnInit {
     }
 
     getUrl() {
-        return 'url(' + this.selectedItem.imgUrl + ')';
+        // tslint:disable-next-line:max-line-length
+        return 'linear-gradient(171deg, rgba(15, 15, 15, 0.6), rgba(14, 14, 14, 0.63) 49%, rgba(11, 11, 11, 0.27) 82%, rgba(6, 6, 6, 0.07)), url(' + this.selectedItem.imgUrl + ')';
     }
 
     getUrlItem(item: ItemCarrousel) {
-        return 'url(' + item.imgUrl + ')';
+        // tslint:disable-next-line:max-line-length
+        return 'linear-gradient(to right, #0f0f0f, rgba(14, 14, 14, 0.84) 67%, rgba(13, 13, 13, 0.64) 84%, rgba(11, 11, 11, 0.35), rgba(6, 6, 6, 0)), url(' + item.imgUrl + ') center / cover';
     }
 }
